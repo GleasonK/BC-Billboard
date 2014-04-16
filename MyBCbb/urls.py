7fee9c8a81c88cf7e849ecdcafd49e77a5e1dc5d
Kevin Gleason <kaagleason@gmail.com>
2014-04-16 13:39:56 -0400

    Configured URLs

--- MyBCbb/urls.py
+++ MyBCbb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+#from MyBCbb import settings
 
 urlpatterns = [
     # Examples:
@@ -9,8 +10,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-urlpatterns += patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
- )
+# urlpatterns += patterns('',
+#     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+#         'document_root': settings.STATIC_ROOT,
+#     }),
+#  )
