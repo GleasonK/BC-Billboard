4ee5184d3f4cff46cc89aea61fce497ff04b0324
andrew <francl@bc.edu>
2014-04-26 16:45:33 -0400

    added testing url for IndexView

--- img_upload/urls.py
+++ img_upload/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from img_upload import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^test/$', views.IndexView.as_view(), name='index'),
     
     # using as view
     # url(r'^helloC/$',  views.HelloTemplate.as_view(), name='HelloC'),
