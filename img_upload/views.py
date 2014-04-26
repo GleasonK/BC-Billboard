a1cab2164bae655fa9ba2bb521a91e2cd94b9e2c
Kevin Gleason <kaagleason@gmail.com>
2014-04-26 18:23:57 -0400

    Fixed views

--- img_upload/views.py
+++ img_upload/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 #from img_upload.models import
-from img_upload.models import Image, Tag, Attending
+from img_upload.models import Image, Tag, Attending, Event
 
 
 
@@ -15,12 +15,12 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		"""Return the most recent 5 uploaded images"""
-		return Image.objects.filter(eDate__gte= timezone.now()).order_by('-eDate')
+		return Event.objects.filter(eDate__gte= timezone.now()).order_by('-eDate')
 
 	def get_context_data(self, **kwargs):
 			# Call the base implementation first to get a context
 			context = super(IndexView, self).get_context_data(**kwargs)
 			# Add in a QuerySet of all the books
-			t = self.get_queryset()
-			context['url'] = Image.get_image_url(context.image)
+			#t = self.get_queryset()
+			#context['url'] = t[0].get_image_url(context.image)
 			return context
