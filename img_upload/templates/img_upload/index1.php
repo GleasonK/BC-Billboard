b1cc95edc30697293d274e0e89dd413bd7fc84d6
Kevin Gleason <kaagleason@gmail.com>
2014-04-26 20:46:19 -0400

    Fixed file upload and file serving

--- img_upload/templates/img_upload/index1.php
+++ img_upload/templates/img_upload/index1.php
@@ -5,11 +5,13 @@
 <body>
 	
 {% for event in recent_images %}
-	<a href="{{event.image.get_image_file_loc}}"><img src="{{event.image.get_medium_image}}" /></a>
+	<a href="{{event.image.get_image_file_loc}}">
+		<img src="{{event.image.get_medium_image}}" />
+	</a>
 	
 	<p>image title: {{event}}</p>
 	<p>image path: {{ event.name }}</p>
-	<p>eDate: {{event.eDate}}</p>
+	<p>Event Date: {{event.eDate}}</p>
 	<p>tags: {{ event.tags_ }}</p>
 	<p>Description: {{ event.description }}</p>
 	<p>User: {{ event.user }}</p>
